0cc594f11bcda4b33e2d9ec826f3595eb69a7f8a
FIRST_NAME <MY_NAME@example.com>
2023-03-12 09:58:19 +0530

    second commit

---
+++ src/test/DeleteExample.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteExample {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		                                                     // url // //username// password//
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "root");
+
+		Statement stmt = con.createStatement();
+
+		stmt.executeUpdate("delete from eproduct where name='Television'");
+		System.out.println("The row is update");
+
+		ResultSet result = stmt.executeQuery("select * from eproduct");
+
+		while (result.next()) {
+
+			System.out.println("Product Id " + result.getInt("ID"));
+			System.out.println("Product name " + result.getString("name"));
+			System.out.println("product price " + result.getDouble("price"));
+		}
+
+		con.close();
+
+	}
+}
